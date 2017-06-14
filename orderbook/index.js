ef0a39f9dde48d3384299891c9d82197b1323d98
Sean Pollock <seanpollock3344@gmail.com>
2017-06-13 21:00:40 -0400

    Instantiating double ended queue for each side of the orderbook.

--- orderbook/index.js
+++ orderbook/index.js
@@ -1,5 +1,17 @@
+import Deque from 'double-ended-queue'
+
 export default class Orderbook {
   constructor() {
     console.log('### made it to Orderbook constructor')
+    this.sellA = new Deque()
+    this.sellB = new Deque()
+  }
+
+  submitSellA() {
+    
+  }
+
+  submitSellB() {
+
   }
 }
