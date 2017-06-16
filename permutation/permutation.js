8d16b1044b3f2902cefdf2c873e492d677341d31
Sean Pollock <seanpollock3344@gmail.com>
2017-06-15 23:55:00 -0400

    Instantiating cleaning class.

--- permutation/permutation.js
+++ permutation/permutation.js
@@ -1,17 +1,13 @@
 import Orderbook from '../orderbook/index'
 import Simulation from '../simulation/index'
 import Match from '../match/index'
-
+import Clean from '../clean/index'
 export default class Permutation {
   constructor() {
     console.log('made it to Permutation constructor')
     this.orderbook = new Orderbook()
-    this.simulation = new Simulation({
-      orderbook: this.orderbook
-    })
-    this.match = new Match({
-      orderbook: this.orderbook
-    })
-    // this.clean
+    this.simulation = new Simulation({ orderbook: this.orderbook })
+    this.match = new Match({ orderbook: this.orderbook })
+    this.clean = new Clean({ orderbook: this.orderbook })
   }
 }
