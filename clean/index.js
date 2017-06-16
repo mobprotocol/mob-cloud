b23bd3d4ce18cf333ecfce0a88f3168f57325170
Sean Pollock <seanpollock3344@gmail.com>
2017-06-16 00:48:50 -0400

    Adding a cleaning daemon that peeks the back element.

--- clean/index.js
+++ clean/index.js
@@ -3,10 +3,21 @@ import Promise from 'bluebird'
 export default class Clean {
   constructor(params) {
     this.orderbook = params.orderbook
+    this.daemon()
   }
 
   daemon() {
-    console.log('hey, hit the cleaning daemon')
+    return new Promise((resolve, reject) => {
+      return Promise.delay(5000)
+      .then(() => {
+        return this.orderbook.sellA.peekBack()
+      }).then((element) => {
+        console.log('Back element', element)
+        return this.daemon()
+      }).catch((err) => {
+        reject(err)
+      })
+    })
   }
 
 }
