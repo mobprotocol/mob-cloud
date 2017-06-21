302718c76b106997bc75b4f4081c312c2a943eae
Sean Pollock <seanpollock3344@gmail.com>
2017-06-21 15:10:31 -0400

    Moving permutation class to index file

--- permutation/index.js
+++ permutation/index.js
@@ -1,5 +1,14 @@
-import Permutation from './permutation'
+import Orderbook from '../orderbook/index'
+import Simulation from '../simulation/index'
+import Match from '../match/index'
+import Clean from '../clean/index'
 
-export function createPermutation() {
-  return new Permutation()
+export default class Permutation {
+  constructor() {
+    console.log('made it to Permutation constructor')
+    this.orderbook = new Orderbook()
+    this.simulation = new Simulation({ orderbook: this.orderbook })
+    this.match = new Match({ orderbook: this.orderbook })
+    this.clean = new Clean({ orderbook: this.orderbook })
+  }
 }
