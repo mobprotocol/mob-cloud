52976ea3de4de609115c749c956ee47f34561914
Sean Pollock <seanpollock3344@gmail.com>
2017-06-21 15:02:07 -0400

    Adding data pushes from orderbook appends

--- orderbook/index.js
+++ orderbook/index.js
@@ -1,5 +1,7 @@
 import Deque from 'double-ended-queue'
 
+import { sendData } from '../socket/index'
+
 export default class Orderbook {
   constructor() {
     console.log('### made it to Orderbook constructor')
@@ -9,11 +11,17 @@ export default class Orderbook {
 
   submitSellA(order) {
     console.log('### submitting sellA order', order)
+    sendData({
+      sellA: order
+    })
     this.sellA.push(order)
   }
 
   submitSellB(order) {
     console.log('### submitting sellB order', order)
+    sendData({
+      sellB: order
+    })
     this.sellB.push(order)
   }
 }
