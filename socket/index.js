7327ac5e7a28af38469238b28543ac1d0067fa01
Sean Pollock <seanpollock3344@gmail.com>
2017-06-21 13:47:33 -0400

    server side of the socket engaged

--- socket/index.js
+++ socket/index.js
@@ -1,8 +1,8 @@
 import Server from 'simple-websocket/server'
 
-const websocket = new Socket({ port: 3001 })
+const socket = new Server({ port: 3001 })
 
-websocket.on('connection', (socket) => {
+socket.on('connection', (socket) => {
   console.log('### WEBSOCKET CONNECTED ON PORT 3001')
   socket.on('data', (data) => {
     console.log('### RECEIVED DATA FROM CLIENT', data)
