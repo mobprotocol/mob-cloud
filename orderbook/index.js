5b4b0cf4dd71a290b192cbaa1174fa0e900ec92d
Sean Pollock <seanpollock3344@gmail.com>
2017-06-22 16:52:41 -0400

    Creating sublevel partition for orderbook data.

--- orderbook/index.js
+++ orderbook/index.js
@@ -1,27 +1,30 @@
 import Deque from 'double-ended-queue'
-
-import { sendData } from '../socket/index'
+import rp from 'request-promise'
+import sublevel from 'level-sublevel'
 
 export default class Orderbook {
-  constructor() {
+  constructor(params) {
     console.log('### made it to Orderbook constructor')
     this.sellA = new Deque()
     this.sellB = new Deque()
+    this.db = params.db.sublevel(`orderbook_${params.tokenA}_params${params.tokenB}`)
   }
 
   submitSellA(order) {
     console.log('### submitting sellA order', order)
-    sendData({
-      sellA: order
-    })
     this.sellA.push(order)
+  }
+
+  insertionSortA() {
+
+  }
+
+  insertionSortB() {
+
   }
 
   submitSellB(order) {
     console.log('### submitting sellB order', order)
-    sendData({
-      sellB: order
-    })
     this.sellB.push(order)
   }
 }
