1be2acd7898a62ee324b4908d3d13b1bb520075c
Sean Pollock <seanpollock3344@gmail.com>
2017-06-22 14:35:42 -0400

    Migrating over to es6 class based approach.

--- network/index.js
+++ network/index.js
@@ -1,37 +1,54 @@
 import Promise from 'bluebird'
 
-import { db } from '../api/index'
 import Permutation from '../permutation/index'
 import { contracts} from '../factory/contracts'
 
-export function fullyConnectedTopology() {
-  return new Promise((resolve, reject) => {
-    let permutations = []
-    return Promise.delay(0)
-    .then(() => {
-      return Object.keys(contracts)
-    }).map((contract) => {
-      const baseContract = contract
-      return Object.keys(contracts).map((contract) => {
-        if (contract > baseContract) {
-          permutations.push({
-            tokenA: baseContract,
-            tokenB: contract
-          })
-        }
+class Network {
+  constructor() {
+    this.permutations = {}
+    this.createNetwork()
+  }
+
+  getConnectedTopology() {
+    return new Promise((resolve, reject) => {
+      let permutations = []
+      return Promise.delay(0)
+      .then(() => {
+        return Object.keys(contracts)
+      }).map((contract) => {
+        const baseContract = contract
+        return Object.keys(contracts).map((contract) => {
+          if (contract > baseContract) {
+            permutations.push({
+              tokenA: baseContract,
+              tokenB: contract
+            })
+          }
+        })
+      }).then(() => {
+        console.log('permutations', permutations.length)
+        resolve(permutations)
+      }).catch((err) => {
+        reject(err)
       })
-    }).then(() => {
-      console.log('permutations', permutations.length)
-      console.log('made it')
-      resolve(true)
-    }).catch((err) => {
-      reject(err)
     })
-  })
+  }
+
+  createNetwork() {
+    return new Promise((resolve, reject) => {
+      return Promise.delay(0)
+      .then(() => {
+        return this.getConnectedTopology()
+      }).map((permutation) => {
+        console.log('### CREATING PERMUTATION FOR TOKEN PAIR', contracts[permutation.tokenA].name, contracts[permutation.tokenB].name)
+        this.permutations[`${contracts[permutation.tokenA].name}_${contracts[permutations.tokenB]}`] = new Permutation()
+      }).then(() => {
+        resolve(true)
+      }).catch((err) => {
+        reject(err)
+      })
+    })
+  }
 }
 
-
-
-
-
-fullyConnectedTopology()
+const network = new Network()
