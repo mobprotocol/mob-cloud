13a02f36756d14619499acb7be02b9ebd2ca6dd0
Sean Pollock <seanpollock3344@gmail.com>
2017-06-22 19:11:51 -0400

    Instantiating settlement class per permutation.

--- permutation/index.js
+++ permutation/index.js
@@ -2,11 +2,12 @@ import Orderbook from '../orderbook/index'
 import Simulation from '../simulation/index'
 import Match from '../match/index'
 import Clean from '../clean/index'
+import Settlement from '../settlement/index'
 
 export default class Permutation {
   constructor(params) {
-    console.log('params', params)
-    this.orderbook = new Orderbook()
+    this.orderbook = new Orderbook({ tokenA: params.tokenA, tokenB: params.tokenB, db: params.db })
+    this.settlment = new Settlement({ tokenA: params, tokenB: params.tokenB, db: params.db })
     this.simulation = new Simulation({ orderbook: this.orderbook })
     this.match = new Match({ orderbook: this.orderbook })
     this.clean = new Clean({ orderbook: this.orderbook })
