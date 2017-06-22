95c8988073b9f1bca9e17ce88f9d18e3811630e7
Sean Pollock <seanpollock3344@gmail.com>
2017-06-22 13:07:16 -0400

    Iterating through token contracts

--- network/index.js
+++ network/index.js
@@ -1,5 +1,24 @@
 import Promise from 'bluebird'
 
+import { db } from '../api/index'
 import Permutation from '../permutation/index'
+import { contracts} from '../factory/contracts'
 
-const market = new Permutation()
+export function fullyConnectedTopology() {
+  return new Promise((resolve, reject) => {
+    return Promise.delay(0)
+    .then(() => {
+      return Object.keys(contracts)
+    }).map((contract) => {
+      console.log('contract', contract)
+      return contract.ticker
+    }).then(() => {
+      console.log('made it')
+      resolve(true)
+    }).catch((err) => {
+      reject(err)
+    })
+  })
+}
+
+fullyConnectedTopology()
