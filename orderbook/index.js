e52e73467757b9e2a34319050cf2b2e1eeb591e6
Sean Pollock <seanpollock3344@gmail.com>
2017-06-23 16:32:36 -0400

    Switching from array to immutable list

--- orderbook/index.js
+++ orderbook/index.js
@@ -1,37 +1,39 @@
 import Deque from 'double-ended-queue'
 import rp from 'request-promise'
 import sublevel from 'level-sublevel'
+import { List } from 'immutable'
 
 export default class Orderbook {
   constructor(params) {
     console.log('### made it to Orderbook constructor')
     this.hello = 'world'
-    this.sellA = []
-    this.sellB = []
+    this.sellA = List()
+    this.sellB = List()
     // this.db = params.db.sublevel(`orderbook_${params.tokenA}_params${params.tokenB}`)
   }
 
   submitSellA(order) {
     console.log('### submitting sellA order', order)
-    this.sellA.push(order)
-    this.insertionSortA()
-  }
-
-  insertionSortA(order) {
-    for (const order in this.sellA) {
-      console.log('order', order)
+    let index
+    console.log("this.sellA", this.sellA)
+    for (const entry in this.sellA) {
+      if (this.sellA[entry].price < order.price) {
+        index = entry
+        break
+      }
     }
-  }
-
-  insertionSortB() {
-    for (const order in this.sellB) {
-      console.log('order', order)
-    }
+    this.sellA.splice(index, 0, order)
   }
 
   submitSellB(order) {
     console.log('### submitting sellB order', order)
-    this.sellB.push(order)
-    this.insertionSortB()
+    let index
+    for (const entry in this.sellB) {
+      if (this.sellB[entry].price < order.price) {
+        index = entry
+        break
+      }
+    }
+    this.sellB.splice(index, 0, order)
   }
 }
