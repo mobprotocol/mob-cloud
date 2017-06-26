03d5c6d8c7c29df9e0850cf47d1b5dcf0618c6e1
Sean Pollock <seanpollock3344@gmail.com>
2017-06-26 12:01:56 -0400

    Adding daemon to settlement queue

--- settlement/index.js
+++ settlement/index.js
@@ -1,9 +1,29 @@
+import Promise from 'bluebird'
 import sublevel from 'level-sublevel'
 import Trie from 'merkle-patricia-tree'
+import { List } from 'immutable'
 
 export default class Settlement {
   constructor(params) {
     this.db = params.db.sublevel(`settlement_${params.tokenA}_${params.tokenB}`)
     this.trie = new Trie(this.db)
+    this.queue = new List()
+  }
+
+  daemon() {
+    return new Promise((resolve, reject) => {
+      return Promise.delay(0)
+      .then(() => {
+        return this.queue.last()
+      }).then((orders) => {
+        return executeTrades(orders)
+      }).then(() => {
+        return this.queue = this.queue.pop()
+      })
+    })
+  }
+
+  executeTrades(orders) {
+
   }
 }
