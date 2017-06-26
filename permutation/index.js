eaba849f52894b403c1aac26859d08e751ba40bc
Sean Pollock <seanpollock3344@gmail.com>
2017-06-26 15:54:54 -0400

    Passing settlement class into match class

--- permutation/index.js
+++ permutation/index.js
@@ -7,9 +7,9 @@ import Settlement from '../settlement/index'
 export default class Permutation {
   constructor(params) {
     this.orderbook = new Orderbook({ tokenA: params.tokenA, tokenB: params.tokenB, db: params.db })
-    this.settlment = new Settlement({ tokenA: params, tokenB: params.tokenB, db: params.db })
+    this.settlement = new Settlement({ tokenA: params, tokenB: params.tokenB, db: params.db })
     this.simulation = new Simulation({ orderbook: this.orderbook })
-    this.match = new Match({ orderbook: this.orderbook })
+    this.match = new Match({ orderbook: this.orderbook, settlement: this.settlement })
     this.clean = new Clean({ orderbook: this.orderbook })
   }
 }
