b6da40f619327ca5535c2537c1027e0d75aff398
Sean Pollock <seanpollock3344@gmail.com>
2017-06-26 12:21:49 -0400

    importing helper methods from orderbook tests

--- match/test.js
+++ match/test.js
@@ -2,13 +2,42 @@ import Promise from 'bluebird'
 
 import Match from './index'
 import Orderbook from '../orderbook/index'
+import { testSortA, testSortB, generateOrder } from '../orderbook/test'
 
 let orderbook
 let matchAgent
 
+let batch_a_amount = 10
+let batch_b_amount = 10
+
 Promise.delay(0)
 .then(() => {
-  return
+  return orderbook = new Orderbook()
 }).then(() => {
-  return startMatchingEngine()
+  return fillOrderbook()
+}).then(() => {
+  console.log('orderbook', )
+  orderbook.sellA.forEach((element) => {
+    console.log('element', element)
+  })
+  return true
+}).then(() => {
+
+}).catch((err) => {
+  console.log('err', err)
 })
+
+export function fillOrderbook() {
+  return new Promise((resolve, reject) => {
+    return Promise.delay(0)
+    .then(() => {
+      return testSortA()
+    }).then(() => {
+      return testSortB()
+    }).then(() => {
+      resolve(true)
+    }).catch((err) => {
+      reject(err)
+    })
+  })
+}
