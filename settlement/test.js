d4f280e0381abd27379df81f10aefa1572ba880d
Sean Pollock <seanpollock3344@gmail.com>
2017-06-29 13:16:52 -0400

    Simple test case to start daemon

---
+++ settlement/test.js
@@ -0,0 +1,17 @@
+import Promise from 'bluebird'
+
+import Settlement from './Settlement'
+
+let settlement
+
+Promise.delay(0)
+.then(() => {
+  return settlement = new Settlement({
+    tokenA: '0x6846e948d8b1ec25bb99dedf821b0d658e226595',
+    tokenB: '0x2da664251cdff1ef96471d5570d6b7d3687b4516'
+  })
+}).then(() => {
+  Promise.resolve(true)
+}).catch((err) => {
+  Promise.reject(err)
+})
