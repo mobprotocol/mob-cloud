9a48a87968a841a43b26c537c7a9695c284d173a
Sean Pollock <seanpollock3344@gmail.com>
2017-07-03 15:10:57 -0400

    restructuring web3 lib

--- web3/index.js
+++ web3/index.js
@@ -1,4 +1,17 @@
-import Web3Interface from './web3'
+import Web3 from 'web3'
+import Promise from 'buebird'
+const fs = Promise.promisifyAll(require('fs'))
+const jsonfile = Promise.promisifyAll(require('jsonfile'))
+
+class Web3Interface {
+  constructor() {
+    this.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545")
+  }
+
+  deploy() {
+
+  }
+}
 
 module.exports = {
   Web3Interface: new Web3Interface()
