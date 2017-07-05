fd451868323fc3dcb73dd731bf3b571b28d0335b
Sean Pollock <seanpollock3344@gmail.com>
2017-07-05 14:47:36 -0400

    Adding global memory object to house class instantiations.

--- users/swarm.js
+++ users/swarm.js
@@ -1,13 +1,24 @@
 import Promise from 'bluebird'
-import keythereum from 'keythereum'
 
+import User from './index'
+
+let operator
+const users = {}
+const swarm_amount = 100
 
 export function exchangeOperator() {
-
+  return new Promise((resolve, reject) => {
+    return Promise.delay(0)
+    .then(() => {
+      return operator = new User()
+    })
+  })
 }
 
 export function traders() {
+  return new Promise((resolve, reject) => {
 
+  })
 }
 
 export function createAccounts() {
