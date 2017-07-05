098371e4c977902ef1aa6b51896da182601e848f
Sean Pollock <seanpollock3344@gmail.com>
2017-07-05 15:07:07 -0400

    succesfully creating accounts for users

--- users/index.js
+++ users/index.js
@@ -4,11 +4,21 @@ import keythereum from 'keythereum'
 export default class User {
   constructor(name) {
     console.log('### creating User instance', name)
+    this.name = name
     this.address
+    this.generateAddress()
   }
 
   generateAddress() {
-
+    return new Promise((resolve, reject) => {
+      this.dk = keythereum.create({ keyBytes: 32, ivBytes: 16 }, (res) => {
+          this.dk = res
+          console.log('privateKey', this.dk.privateKey.toString('utf-8'))
+          console.log('iv', this.dk.iv.toString('utf-8'))
+          console.log('salt', this.dk.salt.toString('utf-8'))
+          resolve(true)
+      })
+    })
   }
 
 }
