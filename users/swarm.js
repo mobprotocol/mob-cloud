8540074842b68af406209d84e0a89dd7be8d4cdd
Sean Pollock <seanpollock3344@gmail.com>
2017-07-05 14:51:33 -0400

    Adding recursive script to create 100 trader accounts

--- users/swarm.js
+++ users/swarm.js
@@ -3,8 +3,8 @@ import Promise from 'bluebird'
 import User from './index'
 
 let operator
-const users = {}
-const swarm_amount = 100
+let users = []
+let swarm_amount = 100
 
 export function exchangeOperator() {
   return new Promise((resolve, reject) => {
@@ -17,7 +17,19 @@ export function exchangeOperator() {
 
 export function traders() {
   return new Promise((resolve, reject) => {
-
+    return Promise.delay(500)
+    .then(() => {
+      return users[swarm_amount] = new User()
+    }).then(() => {
+      if (swarm_amount <= 0) {
+        resolve(true)
+      } else {
+        swarm_amount = swarm_amount - 1
+        return traders()
+      }
+    }).catch((err) => {
+      reject(err)
+    })
   })
 }
 
