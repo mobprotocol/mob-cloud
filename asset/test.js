b6a6beac241eb63118cf770d3e267e1a7d0086d6
Sean Pollock <seanpollock3344@gmail.com>
2017-07-06 11:59:42 -0400

    creating exchange operator user

--- asset/test.js
+++ asset/test.js
@@ -2,15 +2,21 @@ import Promise from 'bluebird'
 
 import Eths6 from '../eths6/index'
 import Asset from './index'
+import User from '../users/index'
 
+let operator
 let asset
+
 export function createAsset() {
   return new Promise((resolve, reject) => {
     return Promise.delay(0)
     .then(() => {
+      return operator = new User('operator')
+    }).then(() => {
       return asset = new Asset({
         file: 'Asset',
-        deploy: true
+        deploy: true,
+        ownder: operator.publicKey
       })
     }).then(() => {
       resolve(true)
