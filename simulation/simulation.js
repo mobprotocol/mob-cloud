a1141014716028964803342f99427e198049d425
Sean Pollock <seanpollock3344@gmail.com>
2017-07-12 13:42:56 -0400

    Adding simulation loop template

--- simulation/simulation.js
+++ simulation/simulation.js
@@ -1,4 +1,4 @@
-import Orderbook from '../orderbook/Orderbook'
+import Orderbook from '../orderbook/index'
 
 export function oneSimultation() {
   return new Promise((resolve, reject) => {
@@ -6,7 +6,27 @@ export function oneSimultation() {
       tokenA: '0x6846e948d8b1ec25bb99dedf821b0d658e226595',
       tokenB: '0x2da664251cdff1ef96471d5570d6b7d3687b4516'
     })
-    console.log('orderbook', orderbook)
+    return simulationLoop().then((res) => {
+      if(res) {
+        resolve(res)
+      }
+      reject(res)
+    })
+  })
+}
+
+export function simulationLoop() {
+  return new Promise((resolve, reject) => {
+    return Promise.delay(0)
+    .then(() => {
+      return calculateMarketPrice()
+    }).then(() => {
+      return calculateVoluem()
+    }).then(() => {
+      return shotgun()
+    }).then(() => {
+      resolve(true)
+    })
   })
 }
 
