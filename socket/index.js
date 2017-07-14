09defa6861fc93072b2d116908314ceacc9dc7bb
Sean Pollock <seanpollock3344@gmail.com>
2017-07-14 01:54:55 -0400

    simple socket server

--- socket/index.js
+++ socket/index.js
@@ -3,11 +3,20 @@ import Server from 'simple-websocket/server'
 const socket = new Server({ port: 3001 })
 
 socket.on('connection', (socket) => {
-  console.log('### WEBSOCKET CONNECTED ON PORT 3001')
+  console.log('### CLIENT CONNECTED ON PORT 3001')
   socket.on('data', (data) => {
-    console.log('### RECEIVED DATA FROM CLIENT', data)
+    console.log('### RECEIVED DATA FROM CLIENT', data)    
   })
 })
+
+/**
+/////////////////////////////////////////////
+DATA TUNNEL
+
+DATA TYPES: (DATA IS PARSED BY JSON KEY)
+
+/////////////////////////////////////////////
+*/
 
 export function sendData(data) {
   socket.write(data)
