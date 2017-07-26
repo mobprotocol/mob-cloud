543d0fb43ea8cdc3489e0d0fa96fbfa0b72e21ef
Sean Pollock <seanpollock3344@gmail.com>
2017-07-26 11:58:06 -0400

    getting account from web3 instead of generated keythereum

--- factory/index.js
+++ factory/index.js
@@ -2,24 +2,39 @@ import Promise from 'bluebird'
 import { createPermutation } from '../permutation/index'
 import { contracsts, pair } from './contracts'
 import Asset from '../asset/index'
+import Web3 from 'web3'
 
-  // createPermutation('tokenA', 'tokenB')
+const web3 = new Web3()
+web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 
+let account
 export function tokenPair() {
   return new Promise((resolve, reject) => {
-    return Promise.delay(0)
+    return getAccount()
     .then(() => {
+      console.log('pair', pair)
       return pair
     }).map((asset) => {
+      console.log('asset', asset)
       return new Asset({
         name: asset.name,
         ticker: asset.ticker,
-        supply: asset.supply
+        supply: asset.supply,
+        file: 'Asset'
       })
     }).then(() => {
       resolve(true)
     }).catch((err) => {
       reject(err)
+    })
+  })
+}
+
+export function getAccount() {
+  return new Promise((resolve, reject) => {
+    web3.eth.getAccounts((res, err) => {
+      if(err) { reject(err) }
+      resolve(res[0])
     })
   })
 }
