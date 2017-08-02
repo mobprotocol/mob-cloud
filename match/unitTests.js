5af4927bcffa83ecd3c109b41ab617f873f92cda
Sean Pollock <seanpollock3344@gmail.com>
2017-08-02 18:58:08 -0400

    switching over to tape tests. orderbook instance

--- match/unitTests.js
+++ match/unitTests.js
@@ -1,4 +1,15 @@
-import chai, { should } from 'chai'
+import test from 'tape'
+import Match from './index'
+import Orderbook from '../orderbook/index'
+import { db } from '../leveldb/index'
 
-console.log('chai', chai)
-console.log('should', should)
+const tokenA = '0x6846e948d8b1ec25bb99dedf821b0d658e226595'
+const tokenB = '0x2da664251cdff1ef96471d5570d6b7d3687b4516'
+test('should not match given orders out of the market', (t) => {
+  const orderbook = new Orderbook({
+    tokenA,
+    tokenB,
+    db
+  })
+  const match = new
+})
