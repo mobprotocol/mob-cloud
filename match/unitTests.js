ff1f41c244a468cf6d3a1e4c14e937213df10d36
Sean Pollock <seanpollock3344@gmail.com>
2017-08-03 13:50:14 -0400

    starting to refactor tests

--- match/unitTests.js
+++ match/unitTests.js
@@ -1,15 +1,32 @@
 import test from 'tape'
 import Match from './index'
 import Orderbook from '../orderbook/index'
+import Settlement from '../settlement/index'
 import { db } from '../leveldb/index'
 
 const tokenA = '0x6846e948d8b1ec25bb99dedf821b0d658e226595'
 const tokenB = '0x2da664251cdff1ef96471d5570d6b7d3687b4516'
-test('should not match given orders out of the market', (t) => {
-  const orderbook = new Orderbook({
-    tokenA,
-    tokenB,
-    db
-  })
-  const match = new
+
+const orderbook = new Orderbook({
+  tokenA,
+  tokenB,
+  db
 })
+
+const settlement = new Settlement({
+  tokenA,
+  tokenB,
+  db
+})
+
+const matchingAgent = new Match({
+  settlement,
+  orderbook,
+})
+
+// test('should not match given orders out of the market', (t) => {
+//   cons
+// })
+
+console.log('made it here')
+console.log('matchingAgent', matchingAgent)
